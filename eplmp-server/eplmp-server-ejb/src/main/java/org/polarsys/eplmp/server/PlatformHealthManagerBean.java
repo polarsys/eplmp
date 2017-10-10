b519fddf7779e0714aa2edb04d4bd26d61ea84ca
Morgan Guimard <morgan.guimard@docdoku.com>
2017-10-10 15:26:36 +0200

    Add more checks to health check resource

--- eplmp-server/eplmp-server-ejb/src/main/java/org/polarsys/eplmp/server/PlatformHealthManagerBean.java
+++ eplmp-server/eplmp-server-ejb/src/main/java/org/polarsys/eplmp/server/PlatformHealthManagerBean.java
@@ -15,12 +15,16 @@ import org.polarsys.eplmp.core.exceptions.PlatformHealthException;
 import org.polarsys.eplmp.core.services.IBinaryStorageManagerLocal;
 import org.polarsys.eplmp.core.services.IIndexerManagerLocal;
 import org.polarsys.eplmp.core.services.IPlatformHealthManagerLocal;
+import org.polarsys.eplmp.server.resourcegetters.OfficeConfig;
 
 import javax.ejb.Local;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -39,24 +43,86 @@ public class PlatformHealthManagerBean implements IPlatformHealthManagerLocal {
     @Inject
     private IBinaryStorageManagerLocal storageManager;
 
+    @Inject
+    private OfficeConfig officeConfig;
+
+    @Inject
+    private ConfigManager configManager;
+
     @Override
     public void runHealthCheck() throws PlatformHealthException {
+
+        boolean check = true;
+
         // Database check
         try {
             Long one = (Long) em.createNativeQuery("select 1 from dual").getSingleResult();
             if (one != 1) {
-                throw new PlatformHealthException("Cannot reach database.");
+                LOGGER.log(Level.SEVERE, "Database doesn't seem to be reachable");
+                check = false;
             }
         } catch (Exception e) {
             LOGGER.log(Level.SEVERE, "Database doesn't seem to be reachable", e);
-            throw new PlatformHealthException(e);
+            check = false;
         }
 
         // Indexer check
         boolean ping = indexerManager.ping();
         if (!ping) {
-            LOGGER.log(Level.WARNING, "Indexer doesn't seem to be reachable");
-            throw new PlatformHealthException("Cannot reach indexer");
+            LOGGER.log(Level.SEVERE, "Indexer doesn't seem to be reachable");
+            check = false;
+        }
+
+        // LibreOffice check
+        String officeHome = officeConfig.getOfficeHome();
+
+        if (officeHome == null || officeHome.isEmpty()) {
+            LOGGER.log(Level.SEVERE, "Office is not configured");
+            check = false;
+        } else {
+            Path path = Paths.get(officeHome);
+
+            if (!path.toFile().exists()) {
+                LOGGER.log(Level.SEVERE, "Office is not available for given path: " + officeHome);
+                check = false;
+            }
+
+            if (!path.toFile().canExecute()) {
+                LOGGER.log(Level.SEVERE, "Office is not executable");
+                check = false;
+            }
+        }
+
+        // Check for mandatory config
+        String vaultPath = configManager.getVaultPath();
+
+        if (vaultPath == null || vaultPath.isEmpty()) {
+            LOGGER.log(Level.SEVERE, "Vaultpath is not set, you won't be able to upload/download files");
+            check = false;
+        } else {
+            Path path = Paths.get(vaultPath);
+
+            if (!path.toFile().exists()) {
+                LOGGER.log(Level.SEVERE, "Vaultpath is not available for given path: " + vaultPath);
+                check = false;
+            }
+
+            if (!path.toFile().canWrite()) {
+                LOGGER.log(Level.SEVERE, "Vaultpath is not writeable, please set appropriate rights on " + vaultPath);
+                check = false;
+            }
+
+        }
+
+        // Check for optional config
+        String codebase = configManager.getCodebase();
+        if (codebase == null || codebase.isEmpty()) {
+            LOGGER.log(Level.WARNING, "Codebase is not set, if you are using docdoku-web-front with this server you should configure it");
+        }
+
+        if (!check) {
+            LOGGER.log(Level.SEVERE, "Health check didn't pass");
+            throw new PlatformHealthException(Locale.getDefault());
         }
     }
 }
