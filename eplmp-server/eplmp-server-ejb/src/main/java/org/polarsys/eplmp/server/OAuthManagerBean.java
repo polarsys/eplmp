17b4c80ce88886d5d8f90db3cd10240796b916f9
Morgan Guimard <morgan.guimard@docdoku.com>
2017-12-20 12:39:32 +0100

    Add missing licence headers - optimize imports

--- eplmp-server/eplmp-server-ejb/src/main/java/org/polarsys/eplmp/server/OAuthManagerBean.java
+++ eplmp-server/eplmp-server-ejb/src/main/java/org/polarsys/eplmp/server/OAuthManagerBean.java
@@ -4,9 +4,9 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * <p>
+ *
  * Contributors:
- * DocDoku - initial API and implementation
+ *    DocDoku - initial API and implementation
  *******************************************************************************/
 
 package org.polarsys.eplmp.server;
@@ -22,7 +22,6 @@ import org.polarsys.eplmp.core.services.IAccountManagerLocal;
 import org.polarsys.eplmp.core.services.IOAuthManagerLocal;
 import org.polarsys.eplmp.server.dao.OAuthProviderDAO;
 
-import javax.annotation.Resource;
 import javax.annotation.security.DeclareRoles;
 import javax.annotation.security.RolesAllowed;
 import javax.ejb.Local;
@@ -32,7 +31,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 import java.util.Locale;
-import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
--- eplmp-server/eplmp-server-ejb/src/main/java/org/polarsys/eplmp/server/OauthConfig.java
+++ eplmp-server/eplmp-server-ejb/src/main/java/org/polarsys/eplmp/server/OauthConfig.java
@@ -4,9 +4,9 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * <p>
+ *
  * Contributors:
- * DocDoku - initial API and implementation
+ *    DocDoku - initial API and implementation
  *******************************************************************************/
 
 package org.polarsys.eplmp.server;
@@ -14,7 +14,6 @@ package org.polarsys.eplmp.server;
 import org.polarsys.eplmp.core.common.OAuthProvider;
 
 import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
 import javax.enterprise.context.ApplicationScoped;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
