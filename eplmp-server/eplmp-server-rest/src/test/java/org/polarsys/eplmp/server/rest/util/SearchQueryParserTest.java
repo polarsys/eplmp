555cfc99455dc7d1858e5f9275b2c89a865f1364
Hunsu <rabah.meradi@docdoku.com>
2018-07-19 15:14:14 +0200

    Handle search queries with escaped characters

---
+++ eplmp-server/eplmp-server-rest/src/test/java/org/polarsys/eplmp/server/rest/util/SearchQueryParserTest.java
@@ -0,0 +1,62 @@
+package org.polarsys.eplmp.server.rest.util;
+
+import org.junit.Test;
+
+import static org.polarsys.eplmp.core.query.SearchQuery.*;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class SearchQueryParserTest {
+
+    @Test
+    public void shouldParseAttributesThatContainsEscapedCharactersInName() {
+        String query = "TEXT:myname\\;:myValue";
+
+        List<AbstractAttributeQuery> attributes = SearchQueryParser.parseAttributeStringQuery(query);
+
+        assertEquals(1, attributes.size());
+        assertTextAttribute(attributes.get(0), "myname\\;", "myValue");
+
+    }
+
+    @Test
+    public void shouldParseAttributesThatContainsEscapedCharactersInValue() {
+        String query = "TEXT:myname\\;:myV\\;alue";
+
+        List<AbstractAttributeQuery> attributes = SearchQueryParser.parseAttributeStringQuery(query);
+
+        assertEquals(1, attributes.size());
+        assertTextAttribute(attributes.get(0), "myname\\;", "myV\\;alue");
+    }
+
+    @Test
+    public void shouldParseQueryWithMoreThanOneAttributesThatContainsEscapedCharacters() {
+        String query = "TEXT:myname\\;:myV\\;alue;TEXT:myname\\:fgdfg:myvalue";
+
+        List<AbstractAttributeQuery> attributes = SearchQueryParser.parseAttributeStringQuery(query);
+
+        assertEquals(2, attributes.size());
+        assertTextAttribute(attributes.get(0), "myname\\;", "myV\\;alue");
+        assertTextAttribute(attributes.get(1), "myname\\:fgdfg", "myvalue");
+    }
+
+    @Test
+    public void shouldParseQueryWithMoreThanOneAttributesWithoutEscapedCharacters() {
+        String query = "TEXT:myname:myValue;TEXT:myname2:myvalue2";
+
+        List<AbstractAttributeQuery> attributes = SearchQueryParser.parseAttributeStringQuery(query);
+
+        assertEquals(2, attributes.size());
+        assertTextAttribute(attributes.get(0), "myname", "myValue");
+        assertTextAttribute(attributes.get(1), "myname2", "myvalue2");
+    }
+
+    private void assertTextAttribute(AbstractAttributeQuery attribute, String expectedName, String expectedValue) {
+        assertEquals(TextAttributeQuery.class, attribute.getClass());
+        assertEquals(expectedName, attribute.getName());
+        assertEquals(expectedValue, ((TextAttributeQuery) attribute).getTextValue());
+    }
+
+}
